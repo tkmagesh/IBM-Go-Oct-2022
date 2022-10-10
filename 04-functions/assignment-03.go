9f9f3c0e9940566f4940bc91e730c7c93ff2787a
Magesh Kuppan <tkmagesh77@gmail.com>
2022-10-10 16:56:22 +0530

    assignment-03 (solution)

--- 04-functions/assignment-03.go
+++ 04-functions/assignment-03.go
@@ -8,16 +8,9 @@ package main
 import "fmt"
 
 func main() {
-	var userChoice, n1, n2, result int
+	var userChoice, n1, n2 int
 	for {
-		fmt.Println("1. Add")
-		fmt.Println("2. Subtract")
-		fmt.Println("3. Multiply")
-		fmt.Println("4. Divide")
-		fmt.Println("5. Exit")
-		fmt.Println("Enter your choice :")
-		fmt.Scanln(&userChoice)
-
+		userChoice = getUserChoice()
 		if userChoice == 5 {
 			break
 		}
@@ -25,18 +18,56 @@ func main() {
 			fmt.Println("Invalid choice...")
 			continue
 		}
-		fmt.Println("Enter the numbers :")
-		fmt.Scanln(&n1, &n2)
-		switch userChoice {
-		case 1:
-			result = n1 + n2
-		case 2:
-			result = n1 - n2
-		case 3:
-			result = n1 * n2
-		case 4:
-			result = n1 / n2
-		}
-		fmt.Println("Result =", result)
+		n1, n2 = getInputs()
+		processUserChoice(userChoice, n1, n2)
 	}
+}
+
+func getUserChoice() int {
+	var userChoice int
+	fmt.Println("1. Add")
+	fmt.Println("2. Subtract")
+	fmt.Println("3. Multiply")
+	fmt.Println("4. Divide")
+	fmt.Println("5. Exit")
+	fmt.Println("Enter your choice :")
+	fmt.Scanln(&userChoice)
+	return userChoice
+}
+
+func getInputs() (n1 int, n2 int) {
+	fmt.Println("Enter the numbers :")
+	fmt.Scanln(&n1, &n2)
+	return
+}
+
+func add(x, y int) int {
+	return x + y
+}
+
+func subtract(x, y int) int {
+	return x - y
+}
+
+func multiply(x, y int) int {
+	return x * y
+}
+
+func divide(x, y int) int {
+	return x / y
+}
+
+func processUserChoice(userChoice, n1, n2 int) {
+	var result int
+	switch userChoice {
+	case 1:
+		result = add(n1, n2)
+	case 2:
+		result = subtract(n1, n2)
+	case 3:
+		result = multiply(n1, n2)
+	case 4:
+		result = divide(n1, n2)
+	}
+	fmt.Println("Result =", result)
 }
