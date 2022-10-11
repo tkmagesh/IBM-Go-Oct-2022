a568337d25750e8e6eeaf32bed4c3ea65b1069ad
Magesh Kuppan <tkmagesh77@gmail.com>
2022-10-11 15:33:54 +0530

    assignments 05 & 06 (solution)

--- 08-collections/assignment-05.go
+++ 08-collections/assignment-05.go
@@ -3,3 +3,33 @@ Write a program that will print all the prime numbers between 3 to 100
 genPrimeNos() function should generate the prime numbers
 main() function should print the prime numbers
 */
+
+package main
+
+import "fmt"
+
+func main() {
+	//get the prime numbers from the function
+	primeNos := generatePrimes(3, 100)
+	//print the prime numbers
+	fmt.Println(primeNos)
+}
+
+func generatePrimes(start, end int) []int {
+	primeNos := make([]int, 0)
+	for no := start; no <= end; no++ {
+		if isPrime(no) {
+			primeNos = append(primeNos, no)
+		}
+	}
+	return primeNos
+}
+
+func isPrime(no int) bool {
+	for i := 2; i <= (no / 2); i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
+}
