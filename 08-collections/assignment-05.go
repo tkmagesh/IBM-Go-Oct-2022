595d505d7d4a1132999dbdd6921596de8b2a9883
Magesh Kuppan <tkmagesh77@gmail.com>
2022-10-11 14:49:36 +0530

    assignments 05 & 06 (problem statement)

---
+++ 08-collections/assignment-05.go
@@ -0,0 +1,5 @@
+/*
+Write a program that will print all the prime numbers between 3 to 100
+genPrimeNos() function should generate the prime numbers
+main() function should print the prime numbers
+*/
