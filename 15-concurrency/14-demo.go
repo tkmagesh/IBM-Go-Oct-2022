1ca296c37a63cbab85e10a0af01fbc7c5ed7ebaa
Magesh Kuppan <tkmagesh77@gmail.com>
2022-10-13 15:33:38 +0530

    more on channel operations

--- 15-concurrency/14-demo.go
+++ 15-concurrency/14-demo.go
@@ -10,13 +10,17 @@ func main() {
 	//main => print the added result
 	//execute the add function as a goroutine
 	//feel free to modify the add function
-	ch := make(chan int)
-	go add(100, 200, ch)
+
+	ch := add(100, 200)
 	result := <-ch
 	fmt.Println(result)
 }
 
-func add(x, y int, ch chan int) {
-	result := x + y
-	ch <- result
+func add(x, y int) <-chan int {
+	ch := make(chan int)
+	go func() {
+		result := x + y
+		ch <- result
+	}()
+	return ch
 }
