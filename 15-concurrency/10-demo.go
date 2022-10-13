6cf271b26670aacc5cbe06fb9e545c44758dd20c
Magesh Kuppan <tkmagesh77@gmail.com>
2022-10-13 14:31:25 +0530

    assignment

---
+++ 15-concurrency/10-demo.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	//add => should add the number
+	//main => print the added result
+	//execute the add function as a goroutine
+	//feel free to modify the add function
+}
+
+func add(x, y int) int {
+	return x + y
+}
